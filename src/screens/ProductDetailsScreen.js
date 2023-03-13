157ad80067696686b3fb6005034de6d212a4c630
aslam48 <youngfizzy001@gmail.com>
2023-03-13 15:23:59 +0100

    working

--- src/screens/ProductDetailsScreen.js
+++ src/screens/ProductDetailsScreen.js
@@ -1,46 +1,52 @@
-import { StyleSheet,Text, View, FlatList, useWindowDimensions, Image, ScrollView, Pressable} from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  useWindowDimensions,
+  Image,
+  ScrollView,
+  Pressable,
+} from "react-native";
 import { useSelector } from "react-redux";
 
 const ProductDetailsScreen = () => {
-  const product = useSelector((state) => state.products.selectedProduct)
+  const product = useSelector((state) => state.products.selectedProduct);
 
-  const {width} = useWindowDimensions()
+  const { width } = useWindowDimensions();
 
   const addToCart = () => {
-    console.log("aslam")
-  }
+    console.log("aslam");
+  };
   return (
-     <View>
-    <ScrollView>
-      {/* Image Carousel */}
+    <View>
+      <ScrollView>
+        {/* Image Carousel */}
 
-      <FlatList
-        data={product.images}
-        renderItem={({ item }) => (
-          <Image
-            source={{ uri: item }}
-            style={{ width: width, aspectRatio: 1 }}
-          />
-        )}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        pagingEnabled
-      />
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => (
+            <Image
+              source={{ uri: item }}
+              style={{ width: width, aspectRatio: 1 }}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+        />
 
+        <View style={{ padding: 20 }}>
+          {/* Title */}
+          <Text style={styles.title}>{product.name}</Text>
 
-    <View style={{padding: 20}}>
-      {/* Title */}
-      <Text style={styles.title}>{product.name}</Text>
+          {/* Price */}
+          <Text style={styles.price}>${product.price}</Text>
 
-      {/* Price */}
-      <Text style={styles.price}>${product.price}</Text>
-
-
-      {/* Description */}
-      <Text style={styles.description}>{product.description}</Text>
-      </View>
+          {/* Description */}
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
       </ScrollView>
-
 
       {/* Add to cart button */}
       <Pressable onPress={addToCart} style={styles.button}>
@@ -53,41 +59,41 @@ const ProductDetailsScreen = () => {
 };
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize:34,
-        fontWeight: "500",
-        marginVertical: 10,
-    },
+  title: {
+    fontSize: 34,
+    fontWeight: "500",
+    marginVertical: 10,
+  },
 
-    price: {
-        fontWeight: "500",
-        fontSize: 16,
-        letterSpacing:1.5,
-    },
+  price: {
+    fontWeight: "500",
+    fontSize: 16,
+    letterSpacing: 1.5,
+  },
 
-    description: {
-        marginVertical: 10,
-        fontSize:18,
-        lineHeight: 30,
-        fontWeight: "300"
-    },
+  description: {
+    marginVertical: 10,
+    fontSize: 18,
+    lineHeight: 30,
+    fontWeight: "300",
+  },
 
-    button: {
-        position: "absolute",
-        bottom: 30,
-        width: "90%",
-        alignSelf: "center",
-        backgroundColor: "black",
-        padding: 20,
-        borderRadius: 100,
-        alignItems: "center"
-    },
+  button: {
+    position: "absolute",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    backgroundColor: "black",
+    padding: 20,
+    borderRadius: 100,
+    alignItems: "center",
+  },
 
-    buttonText: {
-        color: "white",
-        fontSize:"500",
-        fontSize: 16,
-    }
+  buttonText: {
+    color: "white",
+    fontSize: "500",
+    fontSize: 16,
+  },
 });
 
 export default ProductDetailsScreen;
